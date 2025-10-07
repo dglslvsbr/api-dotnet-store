fb12c132e735fd9a5275721431acca8372c28093
Douglas Alves <dglslvsbr@gmail.com>
2025-10-07 18:48:32 -0300

    implement extension method for IMemoryCache

---
+++ StoreAPI/Extensions/MemoryCacheExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace StoreAPI.Extensions;
+
+public static class MemoryCacheExtension
+{
+    public static async Task<T> GetOrSetAsync<T>(this IMemoryCache memoryCache, string key, Func<Task<T>> expression)
+    {
+        if (!memoryCache.TryGetValue(key, out T? result))
+        {
+            result = await expression();
+            memoryCache.Set(key, result, DefaultCacheOptions);
+            Console.WriteLine("The data was not retrieved from the cache");
+            return result!;
+        }
+        Console.WriteLine("The data was retrieved from the cache");
+        return result!;
+    }
+
+    private static MemoryCacheEntryOptions DefaultCacheOptions => new()
+    {
+        AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(30),
+        SlidingExpiration = TimeSpan.FromMinutes(5)
+    };
+}
