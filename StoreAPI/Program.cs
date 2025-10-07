e485a6eca1a26befe234befa8f48fa0a02c63143
Douglas Alves <dglslvsbr@gmail.com>
2025-10-07 18:43:51 -0300

    refactoring the Program class

--- StoreAPI/Program.cs
+++ StoreAPI/Program.cs
@@ -3,9 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using StoreAPI.AppContext;
+using Newtonsoft.Json;
+using StoreAPI.Context;
 using StoreAPI.Customs;
-using StoreAPI.Exceptions;
+using StoreAPI.Filters;
 using StoreAPI.Interfaces;
 using StoreAPI.Repositories;
 using StoreAPI.Services;
@@ -13,105 +14,112 @@ using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.RateLimiting;
 
-namespace StoreAPI
+namespace StoreAPI;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Variables
+        var configuration = builder.Configuration;
+        var myAllowSpecifOrigin = "http://127.0.0.1:5500";
+
+        // To suppress the automatic model state validation
+        builder.Services.Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
+
+        // Add services to the container.
+        builder.Services.AddControllers(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            options.Filters.Add<CustomValidationFilter>();
+            options.Filters.Add<GlobalExceptionFilter>();
+        })
+        .AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
-            // Variables
-            var configuration = builder.Configuration;
-            var myAllowSpecifOrigin = "http://127.0.0.1:5500";
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-            // To suppress the automatic model state validation
-            builder.Services.Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
+        // Implemented Rate Limiting
+        builder.Services.AddRateLimiter(options => options.AddFixedWindowLimiter("RateLimiter", options =>
+        {
+            options.PermitLimit = 1;
+            options.Window = TimeSpan.FromSeconds(5);
+            options.QueueLimit = 0;
+            options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        }));
 
-            // Add services to the container.
-            builder.Services.AddControllers(options =>
+        // Implemented CORS
+        builder.Services.AddCors(options => options.AddPolicy("AllowCors", options => options.WithOrigins(myAllowSpecifOrigin).AllowAnyHeader().AllowAnyMethod().AllowCredentials()));
+
+        // Implemented authentication
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters()
             {
-                options.Filters.Add<CustomValidationFilter>();
-                options.Filters.Add<GlobalExceptionFilter>();
-            })
-            .AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles)
-            .AddNewtonsoftJson();
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
 
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+                ValidIssuer = configuration["Jwt:Issuer"],
+                ValidAudience = configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!))
+            });
 
-            // Implemented Rate Limiting
-            builder.Services.AddRateLimiter(options => options.AddFixedWindowLimiter("RateLimiter", options =>
-            {
-                options.PermitLimit = 1;
-                options.Window = TimeSpan.FromSeconds(5);
-                options.QueueLimit = 0;
-                options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-            }));
+        // Implemented authorization
+        builder.Services.AddAuthorizationBuilder().AddPolicy("UserOnly", options => options.RequireRole("User"))
+                                                  .AddPolicy("AdminOnly", options => options.RequireRole("Admin"));
 
-            // Implemented CORS
-            builder.Services.AddCors(options => options.AddPolicy("AllowCors", options => options.WithOrigins(myAllowSpecifOrigin).AllowAnyHeader().AllowAnyMethod()));
+        // DbContext configured
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<AppDbContext>(x => x.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
-            // Implemented authentication
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters()
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
+        // Time Life scoped
+        builder.Services.AddScoped<IClientService, ClientService>();
+        builder.Services.AddScoped<IProductService, ProductService>();
+        builder.Services.AddScoped<IOrderService, OrderService>();
+        builder.Services.AddScoped<ICategoryService, CategoryService>();
 
-                    ValidIssuer = configuration["Jwt:Issuer"],
-                    ValidAudience = configuration["Jwt:Audience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!))
-                });
+        builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-            // Implemented authorization
-            builder.Services.AddAuthorizationBuilder().AddPolicy("UserOnly", options => options.RequireRole("User"))
-                                                      .AddPolicy("AdminOnly", options => options.RequireRole("Admin"));
+        builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        builder.Services.AddScoped<IClientRepository, ClientRepository>();
+        builder.Services.AddScoped<IProductRepository, ProductRepository>();
+        builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+        builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
-            // DbContext configured
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-            builder.Services.AddDbContext<AppDbContext>(x => x.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+        builder.Services.AddScoped<ITokenService, TokenJwtService>();
 
-            // Time Life scoped
-            builder.Services.AddScoped<ITokenService, TokenJwtService>();
-            builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-            builder.Services.AddScoped<AppDbContext>();
-            builder.Services.AddScoped(typeof(ISystemCache<>), typeof(MemoryCache<>));
+        // Auto Mapper
+        builder.Services.AddAutoMapper(typeof(Mappings.AutoMapper));
 
-            // Auto Mapper
-            builder.Services.AddAutoMapper(typeof(Mappings.AutoMapper));
+        // Add Memory Cache
+        builder.Services.AddMemoryCache();
 
-            // Add Memory Cache
-            builder.Services.AddMemoryCache();
+        var app = builder.Build();
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseRateLimiter();
-
-            app.UseCors(myAllowSpecifOrigin);
-
-            app.UseAuthentication();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseCors(myAllowSpecifOrigin);
+
+        app.UseRateLimiter();
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
