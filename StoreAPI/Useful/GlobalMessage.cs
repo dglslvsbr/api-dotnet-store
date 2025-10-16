d6a5a65d58ef07e7bdc899f471217dd98b95ee6d
Douglas Alves <dglslvsbr@gmail.com>
2025-10-16 19:22:22 -0300

    create customized responses

--- StoreAPI/Useful/GlobalMessage.cs
+++ StoreAPI/Useful/GlobalMessage.cs
@@ -2,9 +2,10 @@
 
 public abstract class GlobalMessage
 {
-    public static string NotFound404 { get; private set; } = "No data was found.";
-    public static string Conflit409 { get; private set; } = "This data already exists.";
-    public static string BadRequest400 { get; private set; } = "An error occurred in the request.";
-    public static string OK200 { get; private set; } = "Request completed successfully";
-    public static string Created201 { get; set; } = "A new feature has been successfully created.";
+    public static string NotFound404 { get; private set; } = "No data found";
+    public static string Conflit409 { get; private set; } = "Conflict, data already exists";
+    public static string BadRequest400 { get; private set; } = "An error occurred in the request";
+    public static string OK200 { get; private set; } = "Request successful";
+    public static string Created201 { get; set; } = "A new resource has been successfully created";
+    public static string Message(string text) => text;
 }
---
+++ StoreAPI/Useful/ServiceResult.cs
@@ -0,0 +1,11 @@
+﻿namespace StoreAPI.Useful;
+
+public class ServiceResult
+{
+    public bool Success { get; set; }
+    public string? ErrorMessage { get; set; }
+    public Exception? Exception { get; set; }
+
+    public static ServiceResult Ok() => new() { Success = true };
+    public static ServiceResult Fail(string message) => new() { Success = false, ErrorMessage = message };
+}
