99e5131a4827a34cd81a96d3b97272e03d396539
Douglas Alves <dglslvsbr@gmail.com>
2025-10-16 21:01:48 -0300

    add documentation to the authentication controller

--- StoreAPI/Controllers/AuthController.cs
+++ StoreAPI/Controllers/AuthController.cs
@@ -10,54 +10,105 @@ using StoreAPI.Useful;
 
 namespace StoreAPI.Controllers;
 
+/// <summary>
+/// Controller responsible for user authentication and permissions management
+/// </summary>
 [EnableRateLimiting("RateLimiter")]
 [EnableCors("AllowCors")]
 [ApiController]
 [Route("api/[controller]")]
 public class AuthController(IUnitOfWork unitOfWork, IConfiguration configuration, ITokenService tokenService, IMapper mapper, ILogger<AuthController> logger) : ControllerBase
 {
+    /// <summary>
+    /// Logs in a user and returns a JWT token.
+    /// </summary>
+    /// <response code="400">Invalid user</response>
+    /// <response code="404">User does not exist in the database.</response>
+    /// <response code="401">Unauthorized user, password does not match</response>
+    /// <response code="200">User is authenticated successfuly</response>
     [HttpPost]
     [Route("Login")]
     public async Task<IActionResult> LoginAsync([FromBody] LoginDTO loginDTO)
     {
         if (loginDTO is null)
-            return BadRequest(new Response<IActionResult> { StatusCode = StatusCodes.Status400BadRequest, Message = GlobalMessage.BadRequest400 });
+            return BadRequest(new Response<IActionResult>
+            {
+                StatusCode = StatusCodes.Status400BadRequest,
+                Message = GlobalMessage.BadRequest400
+            });
 
         var userExist = await unitOfWork.ClientRepository.GetByEmailAsync(loginDTO.Email!);
 
         if (userExist is null)
-            return BadRequest(new Response<IActionResult> { StatusCode = StatusCodes.Status400BadRequest, Message = GlobalMessage.BadRequest400 });
+            return NotFound(new Response<IActionResult>
+            {
+                StatusCode = StatusCodes.Status400BadRequest,
+                Message = GlobalMessage.NotFound404
+            });
 
         if (!BCrypt.Net.BCrypt.Verify(loginDTO.Password, userExist.Password))
-            return Unauthorized(new Response<IActionResult> { StatusCode = StatusCodes.Status400BadRequest, Message = GlobalMessage.Message("The password does not match") });
+            return Unauthorized(new Response<IActionResult>
+            {
+                StatusCode = StatusCodes.Status400BadRequest,
+                Message = GlobalMessage.Message("The password does not match")
+            });
 
         var token = tokenService.GenerateToken(userExist, configuration);
 
         logger.LogInformation($"AuthController: The Client with ID {userExist.Id} has connected");
-        return Ok(new Response<string> { StatusCode = StatusCodes.Status200OK, Message = GlobalMessage.OK200, Data = token });
+        return Ok(new Response<string>
+        {
+            StatusCode = StatusCodes.Status200OK,
+            Message = GlobalMessage.OK200,
+            Data = token
+        });
     }
 
+    /// <summary>
+    /// Create a new permission/role in the system (Required Admin Role)
+    /// </summary>
+    /// <response code="200">Permission successfully created</response>
+    /// <response code="400">Invalid role</response>
+    /// <response code="409">Conflict, role already exist</response>
     [Authorize(Policy = "AdminOnly")]
     [HttpPost]
     [Route("CreateRole")]
     public async Task<IActionResult> CreateRoleAsync([FromBody] CreateRoleDTO createRoleDto)
     {
         if (createRoleDto is null)
-            return BadRequest(new Response<IActionResult> { StatusCode = StatusCodes.Status400BadRequest, Message = GlobalMessage.BadRequest400 });
+            return BadRequest(new Response<IActionResult>
+            {
+                StatusCode = StatusCodes.Status400BadRequest,
+                Message = GlobalMessage.BadRequest400
+            });
 
         var roleExist = await unitOfWork.RoleRepository.GetByNameAsync(createRoleDto.Name!);
 
         if (roleExist is not null)
-            return Conflict(new Response<IActionResult> { StatusCode = StatusCodes.Status409Conflict, Message = GlobalMessage.Conflit409 });
+            return Conflict(new Response<IActionResult>
+            {
+                StatusCode = StatusCodes.Status409Conflict,
+                Message = GlobalMessage.Conflit409
+            });
 
         var role = mapper.Map<Role>(createRoleDto);
         await unitOfWork.RoleRepository.CreateAsync(role);
         await unitOfWork.CommitAsync();
 
         logger.LogInformation($"AuthController: A Role with ID {role.Id} was created successfully");
-        return Ok(new Response<IActionResult> { StatusCode = StatusCodes.Status201Created, Message = GlobalMessage.Created201 });
+        return Ok(new Response<IActionResult>
+        {
+            StatusCode = StatusCodes.Status201Created,
+            Message = GlobalMessage.Created201
+        });
     }
 
+    /// <summary>
+    /// Add a permission/role to a user (Required Admin Role)
+    /// </summary>
+    /// <response code="200">Permission successfully added to the user</response>
+    /// <response code="404">User not found or role not found</response>
+    /// <response code="409">User already has permission</response>
     [Authorize(Policy = "AdminOnly")]
     [HttpPost]
     [Route("AddRole/{email}/{roleName}")]
@@ -67,23 +118,48 @@ public class AuthController(IUnitOfWork unitOfWork, IConfiguration configuration
         var roleExist = await unitOfWork.RoleRepository.GetByNameAsync(roleName);
 
         if (clientExist is null)
-            return NotFound(new Response<IActionResult> { StatusCode = StatusCodes.Status404NotFound, Message = GlobalMessage.NotFound404 });
+            return NotFound(new Response<IActionResult>
+            {
+                StatusCode = StatusCodes.Status404NotFound,
+                Message = GlobalMessage.NotFound404
+            });
 
         if (roleExist is null)
-            return NotFound(new Response<IActionResult> { StatusCode = StatusCodes.Status404NotFound, Message = GlobalMessage.NotFound404 });
+            return NotFound(new Response<IActionResult>
+            {
+                StatusCode = StatusCodes.Status404NotFound,
+                Message = GlobalMessage.NotFound404
+            });
 
         foreach (var obj in clientExist.ClientRole!)
             if (obj.Role!.Name == roleName)
-                return BadRequest(new Response<IActionResult> { StatusCode = StatusCodes.Status400BadRequest, Message = GlobalMessage.BadRequest400 });
+                return Conflict(new Response<IActionResult>
+                {
+                    StatusCode = StatusCodes.Status409Conflict,
+                    Message = GlobalMessage.Conflit409
+                });
 
-        clientExist.ClientRole.Add(new ClientRole { RoleId = roleExist.Id, ClientId = clientExist.Id });
+        clientExist.ClientRole.Add(new ClientRole
+        {
+            RoleId = roleExist.Id,
+            ClientId = clientExist.Id
+        });
 
         await unitOfWork.CommitAsync();
 
         logger.LogInformation($"AuthController: The Role {roleExist.Name} was assigned to Client with ID: {clientExist.Id}");
-        return Ok(new Response<IActionResult> { StatusCode = StatusCodes.Status200OK, Message = GlobalMessage.OK200 });
+        return Ok(new Response<IActionResult>
+        {
+            StatusCode = StatusCodes.Status200OK,
+            Message = GlobalMessage.OK200
+        });
     }
 
+    /// <summary>
+    /// Delete a system permission/role (Required Admin Role)
+    /// </summary>
+    /// <response code="200">Permission was deleted successfully</response>
+    /// <response code="404">Permission not found</response>
     [Authorize(Policy = "AdminOnly")]
     [HttpDelete]
     [Route("DeleteRole/{id:int}")]
@@ -92,11 +168,19 @@ public class AuthController(IUnitOfWork unitOfWork, IConfiguration configuration
         var roleExist = await unitOfWork.RoleRepository.GetAsync(id);
 
         if (roleExist is null)
-            return NotFound(new Response<IActionResult> { StatusCode = StatusCodes.Status404NotFound, Message = GlobalMessage.NotFound404 });
+            return NotFound(new Response<IActionResult>
+            {
+                StatusCode = StatusCodes.Status404NotFound,
+                Message = GlobalMessage.NotFound404
+            });
 
         unitOfWork.RoleRepository.Delete(roleExist);
 
         logger.LogInformation($"The Role with ID: {roleExist.Id} was deleted successfully");
-        return Ok(new Response<IActionResult> { StatusCode = StatusCodes.Status200OK, Message = GlobalMessage.OK200 });
+        return Ok(new Response<IActionResult>
+        {
+            StatusCode = StatusCodes.Status200OK,
+            Message = GlobalMessage.OK200
+        });
     }
 }
